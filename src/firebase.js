75f70d70bf75721bc5fc71551877f45a64223adb
Theo <genaddress@gmail.com>
2024-08-26 16:58:51 +0200

    cleanup

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,12 @@
 import { provide } from "vue";
-
 import { initializeApp } from 'firebase/app'
 import * as firestore from 'firebase/firestore'
 
-// ... other firebase imports
+import { firebaseCredentials } from '../security/firebase-credentials.js'
 
-const firebaseApp = initializeApp({
-  apiKey: 'AIzaSyDICIrxQCMOPzfDnRo1XS4ScoKyc5_1n0Y',
-  authDomain: 'connected-mirror-91cb7.firebaseapp.com',
-  projectId: 'connected-mirror-91cb7',
-  storageBucket: 'connected-mirror-91cb7.appspot.com',
-  messagingSenderId: '111172689510',
-  appId: '1:111172689510:web:aee0b505696a7e89a897a7',
-  measurementId: 'G-X34HXJ0LQ2'
-})
 
+
+const firebaseApp = initializeApp(firebaseCredentials)
 
 export default {
 
